c3f6a34dad4c0774ee6da667be5a126f2a475c6f
Bradford Toney <bradford.toney@gmail.com>
2012-09-03 13:25:22 -0700

    Performance

--- mongoengine/optimized.pyx
+++ mongoengine/optimized.pyx
@@ -1,3 +1,7 @@
+from bson.dbref import DBRef
+import operator
+
+
 def _leading_zero(number):
     """
     Converts the given number to a string.
@@ -25,3 +29,55 @@ def _convert_from_datetime(cls, val):
     for name in cls.names:
         data.append(_leading_zero(getattr(val, name)))
     return ','.join(data)
+
+
+
+
+def complex_base_field_to_mongo(cls, value):
+    """Convert a Python type to a MongoDB-compatible type.
+    """
+    from mongoengine import Document
+
+    if isinstance(value, basestring):
+        return value
+
+    if hasattr(value, 'to_mongo'):
+        return value.to_mongo()
+
+    is_list = False
+    if not hasattr(value, 'items'):
+        try:
+            is_list = True
+            value = dict([(k, v) for k, v in enumerate(value)])
+        except TypeError:  # Not iterable return the value
+            return value
+
+    if cls.field:
+        value_dict = dict([(key, cls.field.to_mongo(item)) for key, item in value.items()])
+    else:
+        value_dict = {}
+        for k, v in value.items():
+            if isinstance(v, Document):
+                # We need the id from the saved object to create the DBRef
+                if v.pk is None:
+                    cls.error('You can only reference documents once they'
+                               ' have been saved to the database')
+
+                # If its a document that is not inheritable it won't have
+                # _types / _cls data so make it a generic reference allows
+                # us to dereference
+                meta = getattr(v, 'meta', getattr(v, '_meta', {}))
+                if meta and not meta.get('allow_inheritance', True) and not cls.field:
+                    from fields import GenericReferenceField
+                    value_dict[k] = GenericReferenceField().to_mongo(v)
+                else:
+                    collection = v._get_collection_name()
+                    value_dict[k] = DBRef(collection, v.pk)
+            elif hasattr(v, 'to_mongo'):
+                value_dict[k] = v.to_mongo()
+            else:
+                value_dict[k] = cls.to_mongo(v)
+
+    if is_list:  # Convert back to a list
+        return [v for k, v in sorted(value_dict.items(), key=operator.itemgetter(0))]
+    return value_dict
